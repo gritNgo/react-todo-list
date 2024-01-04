08a383895727c9c40ec2521cb380142133169460
gritNgo <dustspeck00@gmail.com>
2024-01-04 23:15:28 +0000

    Basic UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import React, { useState } from 'react'
+
 export default function App() {
-  return <form className="new-item-form">
-  <div></div>
-  </form>;
+  const [newItem, setNewItem] = useState("Item 1");
+  return (
+    <>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor="text" id="item"></label>
+          <input value={newItem} type="text" id="item" />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
