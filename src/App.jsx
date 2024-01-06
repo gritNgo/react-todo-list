63fb3cef3395c0add32797cc56e72d601d04be3a
gritNgo <dustspeck00@gmail.com>
2024-01-06 18:05:45 +0000

    handleSubmit func created to add todo to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,84 @@
 import React, { useState } from 'react'
 
 export default function App() {
-  const [newItem, setNewItem] = useState("Item 1");
+  const [newItem, setNewItem] = useState("");
+  // Any type of data you want to have rerender your component when it changes (ex: input/todos), put in state
+  // if you wanna modify the existing data you need to pass a function to your setTodos useState function
+  // ***YOU CAN'T MANUALLY CHANGE STATE BECAUSE IT'S IMMUTABLE***
+  // Anu time yo uchange state, it needs to create a brand new object and change a property on it
+  const [todos, setTodos] = useState([]);
+   
+  function handleSubmit(e) {
+    // prevents page from refreshing
+    e.preventDefault() 
+
+    // Any time you need to use the CURRENT value you need to pass a function,
+    // otherwise you can pass the value like setNewItem for <input>
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false }, 
+      ]
+    })
+
+    setNewItem("");
+  }
+
+  //update todos to change the id of the todo passed in here to be completed based on the "completed" flag
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        
+        return todo; // if id != id then return todo with no changes at all
+      })
+    })
+  }
+
   return (
     <>
-      <form className='new-item-form'>
+      <form onSubmit={handleSubmit} className='new-item-form'>
         <div className='form-row'>
           <label htmlFor="text" id="item"></label>
-          <input value={newItem} type="text" id="item" />
+          <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)} 
+          type="text" 
+          id="item" 
+          />
         </div>
         <button className='btn'>Add</button>
       </form>
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
-        <li>
+        {todos.map(todo => {
+          return (
+          <li key={todo.id}>
           <label>
-            <input type="checkbox" />
-            Item 1
+            <input 
+            type="checkbox" 
+            checked={todo.completed} 
+            onChange={e => toggleTodo(todo.id, e.target.checked)}
+            />
+            {todo.title}
           </label>
           <button className='btn btn-danger'>Delete</button>
         </li>
+        )
+        })}
       </ul>
     </>
   )
 }
+
+/**
+ * Notes: 
+ * Any time return an array of elements with a .map, each element at the top level
+ * needs to have a key property, unique identifier (id, NOT INDEX). Because when we need to change an element, react needs to 
+ * know which. It's a performance optimisattion that ensures code is fast and bug free.
+ * 
+ * 
+ * 
+ */
